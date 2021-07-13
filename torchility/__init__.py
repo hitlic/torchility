1638eeb2ea40a5d8ac7f61d0de95dcf228de6763
hitlic <liuchen.chn@hotmail.com>
2021-07-13 10:20:35 +0800

    0.1.14

--- torchility/__init__.py
+++ torchility/__init__.py
@@ -1,3 +1,3 @@
 from .trainer import Trainer
 name = 'torchility'
-__version__ = '0.1.13'
+__version__ = '0.1.14'
--- torchility/callbacks/progressbars.py
+++ torchility/callbacks/progressbars.py
@@ -7,11 +7,10 @@ class PrintProgressBar(ProgressBarBase):
     仅输出文本信息的进度条
     """
 
-    def __init__(self, brief=False):
+    def __init__(self):
         self.train_batch_id = 0
         self.val_batch_id = 0
         self.test_batch_id = 0
-        self.brief = brief    # 是否以简短形式输出信息
 
     def disable(self):
         self.enable = True
@@ -32,6 +31,9 @@ class PrintProgressBar(ProgressBarBase):
     def on_validation_epoch_end(self, trainer, pl_module):
         print(self.get_info('val', trainer))
 
+    def on_test_epoch_start(self, trainer, pl_module):
+        print('-' * 10)
+
     def on_test_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         self.test_batch_id = batch_idx + 1
         stdout.write(f"{self.get_info('test', trainer)}\r")
@@ -42,26 +44,26 @@ class PrintProgressBar(ProgressBarBase):
 
     def get_info(self, stage, trainer):
         info = trainer.progress_bar_dict
-        if self.brief:
-            info_str = ' | '.join([f'{self._brief(k):>12}: {str(v)[:4]:4}' for k, v in info.items() if self._check_info(stage, k)])
-        else:
-            info_str = ' | '.join([f'{self._brief(k):>12}: {str(v)[:6]:6}' for k, v in info.items() if self._check_info(stage, k)])
+        info_str = ' | '.join([f'{self._key(k):>8}: {str(v)[:7]:7}' for k, v in info.items() if self._check_info(stage, k)])
         if stage == 'train':
             stage = 'TRAIN'
             c_batch = self.train_batch_id
             num_batch = trainer.num_training_batches
+            loss_str = f'T_loss: {str(info.get("loss","nan"))[:6]:6}'
         elif stage == 'val':
             stage = ' VAL '
             c_batch = self.val_batch_id
             num_batch = self._total_val_batches(trainer)
+            loss_str = f'V_loss: {str(info.get("val_loss", "nan"))[:6]:6}'
         else:
             stage = ' TST '
             c_batch = self.test_batch_id
             num_batch = sum(trainer.num_test_batches)
+            loss_str = f'  loss: {str(info.get("test_loss", "nan"))[:6]:6}'
         c_epoch = trainer.current_epoch + 1
         max_epoch = trainer.max_epochs
 
-        return f"E:{c_epoch:>3}/{max_epoch:<3}|{stage}| B:{c_batch:>4}/{num_batch:<4}| {info_str}"
+        return f"E:{c_epoch:>3}/{max_epoch:<3}|{stage}| B:{c_batch:>4}/{num_batch:<4} | {loss_str} | {info_str}"
 
     def _total_val_batches(self, trainer):
         total_val_batches = 0
@@ -69,19 +71,12 @@ class PrintProgressBar(ProgressBarBase):
         total_val_batches = sum(trainer.num_val_batches) if is_val_epoch else 0
         return total_val_batches
 
-    def _brief(self, k):
-        if self.brief:
-            k_items = k.split('_')
-            if len(k_items) == 1:
-                return k_items[0][:3]
-            x = k_items[0][:3] + '_' + '_'.join([item[0] for item in k_items[1:]])
-            return x.replace('step', 's').replace('epoch', 'e')
-        else:
-            return k.replace('step', 's').replace('epoch', 'e')
+    def _key(self, k):
+        return k.replace('step', 's').replace('epoch', 'e').replace('train', 'T').replace('val', 'V').replace('test_', '')
 
     def _check_info(self, stage, info_key):
-        if info_key == 'loss' and stage == 'train':
-            return True
+        if info_key[-4:] == 'loss':
+            return False
         if info_key.split('_')[0] != stage:
             return False
         else:
